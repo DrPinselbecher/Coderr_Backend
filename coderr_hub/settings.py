537cd77f4dd237d478d5cac5445b9b478ccc55f0
René Theis <rene.tube992@gmail.com>
2026-02-18 19:21:59 +0100

    add CORS configuration for local development frontend

--- coderr_hub/settings.py
+++ coderr_hub/settings.py
@@ -49,16 +49,23 @@ INSTALLED_APPS = [
     'reviews_app',
     'user_auth_app',
     'core',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ]
 
 ROOT_URLCONF = 'coderr_hub.urls'
